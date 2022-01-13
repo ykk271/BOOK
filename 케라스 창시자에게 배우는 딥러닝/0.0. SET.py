b5e167f5ce47a2501018359ef19a9ffada1fcaab
ykk271@gmail.com <ykk271@gmail.com>
2022-01-14 00:28:36 +0900

    .

--- 케라스 창시자에게 배우는 딥러닝/0.0. SET.py
+++ 케라스 창시자에게 배우는 딥러닝/0.0. SET.py
@@ -2,6 +2,9 @@
 from tensorflow.python.client import device_lib
 device_lib.list_local_devices()
 
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+tf.config.list_physical_devices()
 
 # 경고메세지 해결
 import os
