759ebf96b4c9f0cd4e2f2ccf1eebd5674b0b4312
ykk271@gmail.com <ykk271@gmail.com>
2022-03-12 20:36:15 +0900

    .

--- 케라스 창시자에게 배우는 딥러닝/3.6. 주택가격예측_회귀.py
+++ 케라스 창시자에게 배우는 딥러닝/3.6. 주택가격예측_회귀.py
@@ -19,7 +19,7 @@ def build_model():
                            input_shape=(train_data.shape[1],)))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1))
-    model.compile(optimizer='rmspop', loss='mse', metrics=['mae'])
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return model
 
 K = 4
@@ -27,6 +27,30 @@ num_val_samples = len(train_data)//K # // 나눗셈 후 소수점 버림
 num_val_samples
 
 num_epochs = 100
+all_scores=[]
 
-fir i in range(K):
+for i in range(K):
+    print("처리중인 폴드 #", i)
+    val_data = train_data[i * num_val_samples: (i+1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i+1) * num_val_samples]
 
+    partial_tarin_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i+1)*num_val_samples:]],
+        axis=0
+    )
+    partial_tarin_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i+1)*num_val_samples:]],
+        axis=0
+    )
+
+    model = build_model()
+    model.fit(partial_tarin_data, partial_tarin_targets, epochs=num_epochs,
+              batch_size=128)
+    val_mse, val_mae = model.evaluate(val_data, val_targets,verbose=0)
+    all_scores.append(val_mae)
+
+
+
+all_scores
